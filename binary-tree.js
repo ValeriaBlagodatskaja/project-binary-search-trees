115d9bf49f4ad8a4595804821e6f0f50dfc61c04
Valeria48562 <vblagodatskaja@gmail.com>
2023-10-29 11:01:43 +0200

    deleteNode function logic is added

--- binary-tree.js
+++ binary-tree.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(key) {
+    this.key = key;
     this.left = null;
     this.right = null;
   }
@@ -25,6 +25,84 @@ class Tree {
     return newNode;
   }
 
+  insert(key) {
+    this.root = this.insertRec(this.root, key);
+  }
+
+  insertRec(root, key) {
+    if (root === null) {
+      root = new Node(key);
+      return root;
+    }
+
+    if (key < root.key) root.left = this.insertRec(root.left, key);
+    else if (key > root.key) root.right = this.insertRec(root.right, key);
+
+    return root;
+  }
+
+  inorderRec(root) {
+    if (root !== null) {
+      this.inorderRec(root.left);
+      document.write(root.key + "<br/>");
+      this.inorderRec(root.right);
+    }
+  }
+  inorder() {
+    this.inorderRec(this.root);
+  }
+
+  /* Given a binary search tree and a key, this function
+   deletes the key and returns the new root */
+  deleteNode(root, k) {
+    if (root === null) {
+      return root;
+    }
+    // Recursive calls for ancestors of node to be deleted
+    if (root.key > k) {
+      root.left = this.deleteNode(root.left, k);
+      return root;
+    } else if (root.key < k) {
+      root.right = this.deleteNode(root.right, k);
+      return root;
+    }
+    // We reach here when root is the node to be deleted.
+
+    // If one of the children is empty
+    if (root.left === null) {
+      let temp = root.right;
+      root = null;
+      return temp;
+    } else if (root.right === null) {
+      let temp = root.left;
+      root = null;
+      return temp;
+    }
+    // If both children exist
+    else {
+      let succParent = root;
+      // Find successor
+      let succ = root.right;
+      while (succ.left !== null) {
+        succParent = succ;
+        succ = succ.left;
+      }
+      // Delete successor.  Since successor is always left child of its parent we can safely make successor's right
+      // right child as left of its parent.If there is no succ, then assign succ.right to succParent.right
+      if (succParent !== root) {
+        succParent.left = succ.right;
+      } else {
+        succParent.right = succ.right;
+      }
+      // Copy Successor Data to root
+      root.key = succ.key;
+
+      // Delete Successor and return root
+      succ = null;
+      return root;
+    }
+  }
+
   prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
     if (node === null) {
       return;
@@ -36,7 +114,7 @@ class Tree {
         false
       );
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.key}`);
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
@@ -45,4 +123,6 @@ class Tree {
 
 //Test
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+tree.insert(10);
+tree.root = tree.deleteNode(tree.root, 23);
 tree.prettyPrint();
